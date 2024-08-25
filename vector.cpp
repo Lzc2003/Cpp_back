dd745763f46d97e575593f5433c02de8ca877e3f
19458 <19458@DESKTOP-Q5USA4R>
2024-08-25 12:19:08 +0800

    新增

--- vector.cpp
+++ vector.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
-
+#include<vector>
 struct Vertex
 {
 	float x, y, z;
+
+	Vertex(float X,float Y,float Z):x(X), y(Y), z(Z) {
+		std::cout << "copy!!" << std::endl;
+	}
+
 };
 
 std::ostream& operator<<(std::ostream& stream, const Vertex& vertex)
@@ -10,7 +15,28 @@ std::ostream& operator<<(std::ostream& stream, const Vertex& vertex)
 	stream << vertex.x << "," << vertex.y << "," << vertex.z << std::endl;
 	return stream;
 }
+
+static void function(const std::vector<Vertex>& vertices) {
+	
+	//vertices.erase(vertices.begin() + 1);
+	for (Vertex v : vertices)
+	{
+		std::cout << v << std::endl;
+	}
+}
+
 int main()
 {
+	std::vector<Vertex>vertices;
+	vertices.push_back({ 1,2,3 });
+	vertices.push_back({ 4,5,6 });
+
+	function(vertices);
+	vertices.erase(vertices.begin() + 1);
+	for (Vertex v : vertices)
+	{
+		std::cout << v << std::endl;
+	}
+
 	std::cin.get();
 }
